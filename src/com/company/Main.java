2cb6b6169f13ab4433bd2c86b99735d823636d75
” <827851647@qq.com>
2020-02-01 20:15:05 +0800

    交换排序：冒泡和恶快速

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[] a={3,5,9,4,6,7,1,0,5};
+        int[] a1={3,5,9,4,6,7,1,0,5};
+
+        pop.laobiao1pop(a);
+        for (int i = 0; i <a.length ; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    pop.quickSort(a1,0,a1.length-1);
+        for (int i = 0; i <a1.length ; i++) {
+            System.out.print(a1[i]);
+        }
+
+
+    }
+}
+
---
+++ src/com/company/pop.java
@@ -0,0 +1,51 @@
+package com.company;
+
+class pop{
+    //冒泡排序
+    public static void laobiao1pop(int[] a){
+
+        for (int i = 0; i < a.length-1; i++) {
+            for (int j = 0; j <a.length-i-1 ; j++) {
+                if (a[j]>a[j+1])
+                {
+                    int temp=a[j+1];
+                    a[j+1]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+    }
+
+    public static void quickSort(int[] a,int left,int right)
+    {
+        if (right<left)
+            return;
+
+        int base=a[left];
+        int i=left;
+        int j=right;
+
+
+        while (i!=j)
+        {
+            while (a[j]>=base&& i<j)
+            {j--;}
+            while (a[i]<= base && i<j)
+            {i++;}
+
+            //i和j停下交换
+            int temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+
+        }
+        //交换基标和检索到最后的数
+        a[left]=a[i];
+        a[i]=base;
+        //递归解决两边数组的排序
+        //左边
+        quickSort(a,left,i-1);
+        //右边
+        quickSort(a,j+1,right);
+    }
+}
