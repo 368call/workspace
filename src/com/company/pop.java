85e6aacedc193827cbff366d012739a583aa1645
” <827851647@qq.com>
2020-02-02 17:22:26 +0800

    增加插入排序：希尔排序完成

--- src/com/company/pop.java
+++ src/com/company/pop.java
@@ -63,9 +63,49 @@ class pop{
         }
     }
 
-    public static void SheelSort(int []a){
+   /* public static void SheelSort(int []a){
+        for (int h = a.length/2; h >0 ; h/=2) {
+            for (int i = h; i < h; i++) {
+                for (int j = i; j <a.length ; j-=h) {
+                    if (a[j]<a[j-h])
+                    {
+                        int temp=a[j];
+                        a[j]=a[j-h];
+                        a[j-h]=temp;
+                    }
+                }
+            }
+        }
 
 
+    }*/
 
-    }
+
+   public static void shellSort(int[] data)
+   {
+       int j = 0;
+       int temp = 0;
+       //每次将步长缩短为原来的一半
+       for (int increment = data.length / 2; increment > 0; increment /= 2)
+       {
+           for (int i = increment; i < data.length; i++)
+           {
+               temp = data[i];
+               for (j = i; j >= increment; j -= increment)
+               {
+                   if(temp < data[j - increment])//如想从小到大排只需修改这里
+                   {
+                       data[j] = data[j - increment];
+                   }
+                   else
+                   {
+                       break;
+                   }
+
+               }
+               data[j] = temp;
+           }
+
+       }
+   }
 }
